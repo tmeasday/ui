19cc8e73280c9fcc7673b2529bf6ae4a06ea0d7a
Nadia Puypalat <npuypalat@talend.com>
2017-10-17 17:07:42 +0200

    Test: add headerBar assessor

--- packages/components/e2e/component-objects/src/main/java/org/talend/component/AppHeaderBar.java
+++ packages/components/e2e/component-objects/src/main/java/org/talend/component/AppHeaderBar.java
@@ -2,7 +2,9 @@ package org.talend.component;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * AppHeaderBar is used to easy access to WebElements of the react-talend-component AppHeaderBar component.
@@ -12,9 +14,19 @@ public class AppHeaderBar extends Component {
 
     private static final Logger LOGGER = LogManager.getLogger(AppHeaderBar.class);
 
-    static final String NAME = "AppHeaderBar";
+    static final String HEADER_NAME = "AppHeaderBar";
 
-    static final String SELECTOR = ".tc-app-header-bar";
+    static final String HEADER_SELECTOR = ".tc-app-header-bar";
+
+    static final String HELP_SELECTOR = "header-help";
+
+    static final String USER_SELECTOR = "header-user";
+
+    static final String USER_DROP_DOWN_SELECTOR = ".dropdown-menu.dropdown-menu-right";
+
+    static final String ABOUT_SELECTOR = "//li/a[@label='About']";
+
+    static final String LOGOUT_SELECTOR = "//li/a[@label='Logout']";
 
     /**
      * AppHeaderBar constructor
@@ -22,6 +34,26 @@ public class AppHeaderBar extends Component {
      * @param driver Selenium WebDriver
      */
     AppHeaderBar(WebDriver driver) {
-        super(driver, NAME, SELECTOR);
+        super(driver, HEADER_NAME, HEADER_SELECTOR);
+    }
+
+    public WebElement getHelp() {
+        return this.getElement().findElement(By.id(HELP_SELECTOR));
+    }
+
+    public WebElement getUser() {
+        return this.getElement().findElement(By.id(USER_SELECTOR));
+    }
+
+    public WebElement getUserMenu() {
+        return this.getElement().findElement(By.cssSelector(USER_DROP_DOWN_SELECTOR));
+    }
+
+    public WebElement getInfo() {
+        return this.getElement().findElement(By.xpath(ABOUT_SELECTOR));
+    }
+
+    public WebElement getLogout() {
+        return this.getElement().findElement(By.xpath(LOGOUT_SELECTOR));
     }
 }
