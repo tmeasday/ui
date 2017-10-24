ffd1c601ef34dc4d75084016c88144d3d2449476
Nadia Puypalat <npuypalat@talend.com>
2017-10-24 11:55:50 +0200

    Test: Add HeaderBar

--- packages/components/e2e/component-objects/src/main/java/org/talend/component/AppHeaderBar.java
+++ packages/components/e2e/component-objects/src/main/java/org/talend/component/AppHeaderBar.java
@@ -14,19 +14,9 @@ public class AppHeaderBar extends Component {
 
     private static final Logger LOGGER = LogManager.getLogger(AppHeaderBar.class);
 
-    static final String HEADER_NAME = "AppHeaderBar";
+    static final String NAME = "AppHeaderBar";
 
-    static final String HEADER_SELECTOR = ".tc-app-header-bar";
-
-    static final String HELP_SELECTOR = "header-help";
-
-    static final String USER_SELECTOR = "header-user";
-
-    static final String USER_DROP_DOWN_SELECTOR = ".dropdown-menu.dropdown-menu-right";
-
-    static final String ABOUT_SELECTOR = "//li/a[@label='About']";
-
-    static final String LOGOUT_SELECTOR = "//li/a[@label='Logout']";
+    static final String SELECTOR = ".tc-app-header-bar";
 
     /**
      * AppHeaderBar constructor
@@ -34,26 +24,6 @@ public class AppHeaderBar extends Component {
      * @param driver Selenium WebDriver
      */
     AppHeaderBar(WebDriver driver) {
-        super(driver, HEADER_NAME, HEADER_SELECTOR);
-    }
-
-    public WebElement getHelp() {
-        return this.getElement().findElement(By.id(HELP_SELECTOR));
-    }
-
-    public WebElement getUser() {
-        return this.getElement().findElement(By.id(USER_SELECTOR));
-    }
-
-    public WebElement getUserMenu() {
-        return this.getElement().findElement(By.cssSelector(USER_DROP_DOWN_SELECTOR));
-    }
-
-    public WebElement getInfo() {
-        return this.getElement().findElement(By.xpath(ABOUT_SELECTOR));
-    }
-
-    public WebElement getLogout() {
-        return this.getElement().findElement(By.xpath(LOGOUT_SELECTOR));
+        super(driver, NAME, SELECTOR);
     }
 }
