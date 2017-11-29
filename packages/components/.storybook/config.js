b45703da4a08062bab9ec6a5ff6d91f5470b20f9
Tom Coleman <tom@thesnail.org>
2017-11-30 10:44:41 +1100

    Added `react-chromatic` and integrated

--- packages/components/.storybook/config.js
+++ packages/components/.storybook/config.js
@@ -8,6 +8,8 @@ import { configure, setAddon } from '@storybook/react';
 import infoAddon from '@storybook/addon-info';
 import withPropsCombinations from 'react-storybook-addon-props-combinations';
 
+import 'react-chromatic/storybook-addon'
+
 import '@talend/bootstrap-theme/src/theme/theme.scss';
 
 a11y(ReactDOM);
